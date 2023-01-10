eb3c80a1fa4651a66dc87f9ed2e047a92973e2b6
Konrad Szydłowski <k@k.com>
2023-01-10 20:39:07 +0100

    blog client

--- src/main/java/pl/hirely/blogclient/model/client/RetrofitClientConfiguration.java
+++ src/main/java/pl/hirely/blogclient/model/client/RetrofitClientConfiguration.java
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Configuration
 public class RetrofitClientConfiguration {
@@ -14,6 +15,7 @@ public class RetrofitClientConfiguration {
         return new Retrofit.Builder()
                 .baseUrl("http://localhost:8080/")
                 .client(new OkHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
 
